c5a2fdb2d9856caa682f08d4d157ded362e22507
Carla <cm.stacchiola@alumno.um.edu.ar>
2025-09-01 22:47:11 -0300

    empezando class dice

--- backgammon/core/dice.py
+++ backgammon/core/dice.py
@@ -1,3 +1,15 @@
-def dice():
-    ...
+import random 
+
+class dice:
+    def __init__(self):
+        self.values = []
+
+    def roll_single(self):
+        value = random.randint(1,6)
+        self.values = [value]
+        return value
+    
+    
+
+
     

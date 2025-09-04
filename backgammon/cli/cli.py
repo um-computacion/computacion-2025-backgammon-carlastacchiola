3107168f2da4645ea708993daa6f96d9e4859b04
Carla <cm.stacchiola@alumno.um.edu.ar>
2025-09-03 22:42:00 -0300

    cli dice

--- backgammon/cli/cli.py
+++ backgammon/cli/cli.py
@@ -1,2 +1,20 @@
-def cli():
-    ... 
+
+from core.dice import Dice
+
+def main():
+    dice = Dice()
+
+    print("Decidiendo quién comienza la partida...\n")
+
+    jugador1 = dice.roll_single()
+    jugador2 = dice.roll_single()
+
+    print(f"Jugador 1 tiró: {jugador1}")
+    print(f"Jugador 2 tiró: {jugador2}")
+
+    if jugador1 > jugador2:
+        print("El Jugador 1 comienza la partida.")
+        first_turn = [jugador1, jugador2]  
+    elif jugador2 > jugador1:
+        print("El Jugador 2 comienza la partida.")
+        first_turn = [jugador1, jugador2]  

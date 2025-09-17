6140aca3852cbf2105e1d0c984a0a5973d1ab537
Carla <cm.stacchiola@alumno.um.edu.ar>
2025-09-16 22:09:20 -0300

    getters clase player

--- backgammon/core/player.py
+++ backgammon/core/player.py
@@ -1,3 +1,40 @@
-def player():
-    ...
+class player():
+
+    def __init__(self, name: str, color: str ):
+        self.__name__ = name
+        self.__color__ = color
+        self.__checkers__ = 15
+        self.__captured__ = 0
+
+    def get_name(self) -> str:
+        return self.__name__
+    
+    def get_color(self) -> str:
+        return self.__color__
+    
+    def get_checkers(self) ->int:
+        return self.__checkers__
+    
+    def get_captured(self) -> int:
+        return self.__captured__
+
+
+    def capture_checker(self):
+         if self.__checkers__ > 0:
+            self.__checkers__ -= 1
+            self.__captured__ += 1
+
+    def reenter_checker(self):
+        if self.__captured__ > 0:
+            self.__captured__ -= 1
+            self.__checkers__ += 1
+
+    def has_won(self) -> bool:
+        return self.__checkers__ == 0 and self.__captured__ == 0
+
+    def __str__(self):
+        return (f"Jugador {self.__name__} ({self.__color__}) "
+                f"- Fichas: {self.__checkers__}, Capturadas: {self.__captured__}")
+    
+
     
